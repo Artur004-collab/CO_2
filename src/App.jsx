c1b6f05a0ff2ed4a8141fe53f9caf9def37064a4
_004 <art@Arturs-MacBook-Pro.local>
2025-05-20 22:43:02 +0200

    --- Auto Git Commit ---

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CryptoProcessingPage from "./pages/CryptoProcessingPage";
+import Services from "./pages/Services";
 
 export default function App() {
-  return <CryptoProcessingPage />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<CryptoProcessingPage />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+    </Router>
+  );
 }
